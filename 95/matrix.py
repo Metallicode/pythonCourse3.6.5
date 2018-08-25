7d36d5d4eafbb2d9cfe91b7d5322a65ebb7c9d15
Metallicode <41553615+Metallicode@users.noreply.github.com>
2018-08-25 21:03:49 +0300

    25/8

---
+++ 95/matrix.py
@@ -0,0 +1,34 @@
+fruits = ["apple", "rimon", "tapoz", "banana"]
+
+games = [(1,2), (3,0), (2,2), (0,1)]
+
+students = ["haim", "bob", "marie", "bela"]
+
+
+fruit_matrix = [
+      ["apple", "rimon", "tapoz", "banana"],
+      ["anav", "apple", "shezif", "tamar"],
+      ["mango", "anav", "ananas", "melon"],
+      ["apple", "etrog", "egoz", "melon"]]      
+
+game_metrix = [
+      [(1,2), (3,0), (0,2), (0,0)],
+      [(1,4), (2,4), (4,2), (0,3)],
+      [(3,2), (3,2), (2,1), (3,1)],
+      [(1,1), (1,0), (0,1), (2,4)]]
+
+student_metrix = [
+      ["haim", "bob", "marie", "bela"],
+      ["momo", "dani", "irit", "masha"],
+      ["tedy", "ido", "bill", "jon"],
+      ["shlomo", "david", "misha", "ivgeni"]]
+
+student_school = [[
+      ["haim", "bob", "marie", "bela"],
+      ["momo", "dani", "irit", "masha"],
+      ["tedy", "ido", "bill", "jon"],
+      ["shlomo", "david", "misha", "ivgeni"]],
+                  [["rami", "david", "ivan", "moti"],
+                   ["sasha", "gabi", "bili", "noa"],
+                   ["michel", "dorit", "assaf", "miri"],
+                   ["lotem", "esev", "mordi", "genia"]]]
